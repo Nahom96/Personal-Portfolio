fc2af2419d67c9b39053a5a76ace90d44caefa6a
Nahom96 <Nahom.291@outlook.com>
2024-12-18 21:03:59 -0500

    Created a Controller for email sending

---
+++ Personal Website/Controllers/ContactFormController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Personal_Website.Controllers
+{
+	public class ContactFormController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
